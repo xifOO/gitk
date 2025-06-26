3cf28ab88868564f4f3bf4f9383a415de6a7627b
xifOO <wedyi28111@gmail.com>
2025-06-26 17:22:40 +0300

    refactor: remove unused import and optimize clean_diff

--- core/utils.py
+++ core/utils.py
@@ -3,7 +3,6 @@ import re
 import shlex
 import subprocess
 
-from core import config
 
 
 class BashFuntionCaller:
@@ -25,15 +24,17 @@ class BashFuntionCaller:
 
 
 def clean_diff(diff: str) -> str:
+    MAX_DIFF_LENGTH = 3000
+
     if not diff.strip():
         raise ValueError("Diff пустой. Убедитесь что есть изменения для commit.")
 
-    if len(diff) > config.MAX_DIFF_LENGTH:
+    if len(diff) > MAX_DIFF_LENGTH:
         lines = diff.split("\n")
         truncated_lines = []
         count = 0
         for line in lines:
-            if count + len(line) > config.MAX_DIFF_LENGTH - 200:
+            if count + len(line) > MAX_DIFF_LENGTH - 200:
                 break
             truncated_lines.append(line)
             count += len(line)
