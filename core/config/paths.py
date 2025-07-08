a57e8139d4c154381156b26accc4745a1d7d8adb
xifOO <wedyi28111@gmail.com>
2025-07-08 10:23:15 +0300

    feat: add cache directory handler

--- core/config/paths.py
+++ core/config/paths.py
@@ -14,3 +14,16 @@ class ConfigDirectory:
     
     def config_file(self) -> Path:
         return self._base / "config.yaml"
+
+
+class CacheDirectory:
+
+    def __init__(self) -> None:
+        self._config_dir = ConfigDirectory()
+        self._cache_dir = self._config_dir.config_dir() / "cache/providers"
+    
+    def ensure(self) -> None:
+        self._cache_dir.mkdir(parents=True, exist_ok=True)
+    
+    def get_cache_file_path(self, provider_name: str) -> Path:
+        return self._cache_dir / f"{provider_name}_models.json"
