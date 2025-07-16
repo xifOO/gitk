d7a9663cd1fc5baab28cbd2fee149bbec98cdb3e
xifOO <wedyi28111@gmail.com>
2025-07-16 17:39:45 +0300

    refactor: add diff parameter to generate_commit_message

--- core/generator.py
+++ core/generator.py
@@ -1,17 +1,13 @@
 import argparse
-import sys
 
 from core.adapters import ModelFactory
-from core.cli.args_parser import parse_arguments
-from core.cli.cli import ApiKeyCLI, ModelsCLI, TemplatesCLI
 from core.config.config import GitkConfig
 from core.models import Config
 from core.templates import Template
-from core.utils import clean_diff, clean_message, qprint
+from core.utils import clean_diff, clean_message
 
 
-def generate_commit_message(args: argparse.Namespace, config: GitkConfig) -> str:
-    diff = read_diff_from_stdin()
+def generate_commit_message(args: argparse.Namespace, config: GitkConfig, diff: str) -> str:
     cleaned_diff = clean_diff(diff)
 
     config_model: Config = config.load_config()
@@ -38,44 +34,3 @@ def generate_commit_message(args: argparse.Namespace, config: GitkConfig) -> str
 
     return clean_message(commit_message)
     
-
-def read_diff_from_stdin() -> str:
-    if sys.stdin.isatty():
-        raise ValueError("Data must be provided via pipe. Usage: git diff | python generator.py")
-    
-    diff = sys.stdin.read().strip()
-    
-    if not diff:
-        raise ValueError("Empty diff. No changes to analyze.")
-    
-    return diff
-
-
-def main() -> None:
-    try:
-        args = parse_arguments()
-        config = GitkConfig()
-        
-        if args.init:
-            
-            templates_cli = TemplatesCLI()
-            models_cli = ModelsCLI()
-            api_key_cli = ApiKeyCLI()
-
-            selected_model = models_cli.select_model()
-            api_key = api_key_cli.setup_api_key(selected_model)
-            template = templates_cli.setup_interactive()
-
-            config.save_config(selected_model, template, api_key)
-            return
-
-        commit_message = generate_commit_message(args, config)
-
-        qprint(commit_message)
-
-    except Exception as e:
-        raise Exception from e
-    
-
-if __name__ == "__main__":
-    main()
